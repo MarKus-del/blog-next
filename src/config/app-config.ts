a621e0a842f7208407db4f18f4116cf78aea54f2
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-09-04 12:20:13 -0300

    criando variavel de ambiente para url da api

---
+++ src/config/app-config.ts
@@ -0,0 +1,5 @@
+export const API_URL = process.env.API_URL || 'http://localhost:3001';
+export const POST_URL = `${API_URL}/posts`;
+
+export const SITE_NAME = 'Marcos Rodrigues';
+export const SITE_URL = '';
